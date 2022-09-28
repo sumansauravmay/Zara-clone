618c7b0e21cb94271e219dcb1d610509b6e95234
sumansauravmay <sumansauravmay12345@gmail.com>
2022-09-29 01:19:57 +0530

    zara day-2

--- zara/src/App.js
+++ zara/src/App.js
@@ -1,10 +1,10 @@
-
+import Home from "./Main/Home"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Zara</h1>
+     <Home/>
     </div>
   );
 }
---
+++ zara/src/Main/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Navbar from "../day2/Navbar";
+
+function Home(){
+
+    return (
+        <>
+    <Navbar/>
+    
+        </>
+
+    )
+}
+export default Home;
